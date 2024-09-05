58de98eb2d45522a4eb8ffe59beb84642d63eaa5
Ricky Brokaw <rbrokaw@ucsb.edu>
2024-09-05 11:26:58 -0700

    create: a test R file

---
+++ testscript1.R
@@ -0,0 +1,11 @@
+# this is a test script for using GitHub (cloning & staging)
+# it was created using RStudio
+
+# make vector
+x <- c(1,2,3,4,5);
+
+# take a mean
+xm <- mean(x);
+
+# make some more changes
+xsd <- sd(x);
